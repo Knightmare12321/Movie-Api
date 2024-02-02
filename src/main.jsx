6d11340efea8988f188a611c4692b5316bfeb281
Knightmare12321 <qiu4135@gmail.com>
2024-02-02 12:12:00 -0800

    adding pages.jsx and router-dom dependency

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
+import AppRouter from './routers/AppRouter.jsx'
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>,
 )
