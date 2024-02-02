06ddb82b0fc4ff6a946f8b073ef603968503fcac
Knightmare12321 <qiu4135@gmail.com>
2024-02-02 13:38:10 -0800

    add routing to approuter

---
+++ src/components/Nav.jsx
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom";
+
+import "../styles/main-nav.css";
+
+const Nav = () => {
+  function blur(e) {
+    e.target.blur();
+  }
+
+  return (
+    <nav className="main-nav" onClick={blur}>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/favorite">Favorites</NavLink>
+        </li>
+        <li>
+          <NavLink to="/watchlist">Watch Later</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,33 @@
+// Development Components
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Components
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+// Pages
+import PageHome from "../pages/PageHome";
+import PageAbout from "../pages/PageAbout";
+import PageMovie from "../pages/PageMovie";
+import PageFavorite from "../pages/PageFavorite";
+
+function AppRouter() {
+    return (
+      <BrowserRouter>
+        <div className="wrapper">
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<PageHome />} />
+            <Route path="/about" exact element={<PageAbout />} />
+            <Route
+              path="/movie/:id"
+              exact
+              element={<PageMovie />}
+            />
+            <Route path="/favorite" element={<PageFavorite />} />
+            <Route path="/watchlist" element={<PageWatchList />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    );
+  }
