ffb51fe0b9f195e5043b61642ed6873929341b8b
Kenny Qiu <qiu4135@gmail.com>
2024-02-16 20:51:17 -0800

    About Page Content Completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,18 @@
 
 
 const About = () => (
-    <div className="about-container">
+    <div className="about-wrapper">
         <h2>Welcome to MVDB!</h2>
         <section>
-            MVDB is a React JS project proudly created by Kenny and Chuan.
+            <h3>Project Info</h3>
+            <p>Welcome to our movie website, your go-to hub for exploring a vast array of films. </p>
+            <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+            <div className="tmdb-logo"></div>
+        </section>
+        <section>
+            <h3>Team Info</h3>
+            <p>MVDB is a React JS project proudly created by Kenny and Chuan.</p>
+            <p>This dynamic duo of web developers aims to bring together expertise in coding languages, design principles, and a passion for creating seamless user experiences!</p>
         </section>
     </div>
   );
