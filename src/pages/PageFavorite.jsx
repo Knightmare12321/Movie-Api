1d9c4112814a725ab6a40ff154534d3909937c64
Knightmare12321 <qiu4135@gmail.com>
2024-02-20 00:02:00 -0800

    complete fav functionality

--- src/pages/PageFavorite.jsx
+++ src/pages/PageFavorite.jsx
@@ -1,9 +1,40 @@
-
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { appTitle } from "../globals/globals";
+import { useSelector } from "react-redux";
+import MovieCard from "../components/MovieCard";
 
 const PageFavorite = () => {
+    const favs = useSelector((state) => state.fav.favs);
+    console.log(favs);
+    useEffect(() => {
+        document.title = `${appTitle} - Favorites`;
+      }, []);
+
     return (
-        <>
-        </>
+        <main>
+        <section>
+        <h2>My Favourites</h2>
+        {favs == undefined || favs.length < 1 ? (
+          <p>
+            No favourite movies. Return to the <Link to="/">home</Link> page
+            to add some favourite characters.
+          </p>
+        ) : (
+          <div className="movies-grid">
+            {favs.map((obj, i) => {
+              return (
+                <MovieCard
+                  key={i}
+                  movie={obj}
+                  isFav={true}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    </main>
     )
 }
 
